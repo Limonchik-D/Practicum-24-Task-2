51a6a70de32169dedacfd014cff64d9cc2ac8c68
Limonchik David <Limonchik4da@outlook.com>
2025-03-02 12:00:31 +0200

    Добавьте файлы проекта.

---
+++ Avto .cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicum_24_Task_2
+{
+    internal class Avto
+    {
+        private string model;
+        private string color;
+        private double skor;
+
+        public string Model
+        {
+            get { return model == "" ? "Неизвестно" : model; }
+            set { model = value; }
+        }
+        public string Color
+        {
+            get { return color == "" ? "Неизвестно" : model; }
+            set { color = value; } 
+        }
+        public double Skor
+        {
+            get { return skor; }
+            set
+            {
+                if (value >= 60 && value <= 150)
+                {
+                    skor = value;
+                }
+                else
+                {
+                    Console.WriteLine("Скорсоть должна быть от 60 до 150 км/ч");
+                }
+            }
+        }
+        public Avto(string model, string color, double skor)
+        {
+            Model = model;
+            Color = color;
+            Skor = skor;
+        }
+        public override string ToString()
+        {
+            return $"Модель: {Model}\nЦвет: {Color}\nСкорость: {Skor} км/ч";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicum_24_Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Avto avto = new Avto("BMW", "Black", 200);
+            Console.WriteLine(avto.ToString());
+            Console.ReadLine();
+        }
+    }
+}
